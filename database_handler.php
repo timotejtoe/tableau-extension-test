85b737938629c433033a8ca669e6d63427e2157e
Timotej <timotej.toepfer@gmail.com>
2022-12-02 14:41:12 +0100

    multiple edits at once

--- database_handler.php
+++ database_handler.php
@@ -5,8 +5,10 @@ $odbc_driver .= "Server=".$_POST["host"].";Database=".$_POST["database"].";uid="
 $odbc_driver .= ";pwd=".$_POST["password"];
 //echo "<p>$odbc_driver</p>";
 $connection = odbc_connect($odbc_driver, $_POST["username"], $_POST["password"]);
-$query = "UPDATE \"".$_POST["table"]."\" SET \"".$_POST["column"]."\"='".$_POST["new_value"]."'";
-$query .= " WHERE \"Category\"='".$_POST["category"]."'; ";
-echo $query;
-odbc_exec($connection, $query);
+foreach ($_POST["changes"] as $change){
+    $query = "UPDATE \"".$_POST["table"]."\" SET \"".$change["column"]."\"='".$change["new_value"]."'";
+    $query .= " WHERE \"Category\"='".$change["category"]."'; ";
+    echo $query;
+    odbc_exec($connection, $query);
+}
 echo "<p>Updated data.</p>";
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /* globals tableau*/
-//let data_source;
+let data_source;
 let selected_worksheet;
 let database = "KEBOOLA_68";
 let username = "KEBOOLA_WORKSPACE_494363367";
 let password = "C5HPuwoXePNR7vfdxy8s4aMGRnhY9Dn4";
+let request_body = {};
 
 function configure(){
   let defaultValues = JSON.stringify({
@@ -34,64 +35,75 @@ function configure(){
 
 async function cell_change(event) {
   const element = $(event.target);
-  let source_id = element.closest("table").data("source");
-  let sources = await selected_worksheet.getDataSourcesAsync();
-  let source = sources.find(s=>s.id=source_id);
-  let source_table = (await source.getLogicalTablesAsync())[0];
-  let connection_info = (await source.getConnectionSummariesAsync())[0]
-  body={
+  change = {
     category: element.data("category"),
     column: element.data("column"),
-    new_value: element.val(),
-    table: source_table.id.split(" (", 1)[0],
-    schema: source_table.id.split(" (",2)[1].split(".",1)[0],
-    host: connection_info.serverURI,
-    database: database,
-    username,
-    password
+    new_value: element.val()
   }
-  $("#message").html("requested data update");
-  $.post("./database_handler.php", body, function(response){
-    //console.log("post response:", response);
-    source.refreshAsync();
-    $("#message").html(response);
-  });
+  request_body.changes.push(change);
 }
 
 function addSelectionListeners(dashboard){
   for (let worksheet of dashboard.worksheets){
     worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, async (event)=>{
+      makeRequest();
       selected_worksheet = event.sheet;
-      let marks = await event.sheet.getSelectedMarksAsync();
-      console.log(marks);
-      clear_tables();
-      for (let mark of marks.data){
-        source = mark.columns[0].fieldId.match(/\[([a-zA-Z0-9.]+)\]/)[1];
-        headers = [];
-        for(col of mark.columns){
-          headers.push(col.fieldName);
-        }
-        result_data = [];
-        for (let data_row of mark.data){
-          let result_row = []
-          for (let data_cell of data_row){
-            result_row.push(data_cell.formattedValue);
-          }
-          result_data.push(result_row);
-        }
-        show_data_in_table(source, headers, result_data);
+      let markCollections = await event.sheet.getSelectedMarksAsync();
+      console.log(markCollections);
+      let markCollection = markCollections.data[0]
+      source_id = markCollection.columns[0].fieldId.match(/\[([a-zA-Z0-9.]+)\]/)[1];
+      let sources = await selected_worksheet.getDataSourcesAsync();
+      data_source = sources.find(s=>s.id=source_id);
+      let source_table = (await data_source.getLogicalTablesAsync())[0];
+      let connection_info = (await data_source.getConnectionSummariesAsync())[0];
+      request_body={
+        table: source_table.id.split(" (", 1)[0],
+        schema: source_table.id.split(" (",2)[1].split(".",1)[0],
+        host: connection_info.serverURI,
+        database: database,
+        username,
+        password,
+        changes:[]
       }
-      $("#table-container").data("sheet", event.sheet);
+
+      headers = [];
+      for(col of markCollection.columns){
+        headers.push(col.fieldName);
+      }
+      result_data = [];
+      for (let data_row of markCollection.data){
+        let result_row = []
+        for (let data_cell of data_row){
+          result_row.push(data_cell.formattedValue);
+        }
+        result_data.push(result_row);
+      }
+      show_data_in_table(headers, result_data);
+    
       $("#table-container table input").change(cell_change);
     });
   }
 }
 
-function clear_tables(){
+function makeRequest(){
+  if (request_body && request_body.changes && request_body.changes.length>0){
+    $("#message").html("requested data update");
+    $.post("./database_handler.php", request_body, (response)=>{
+      //console.log("post response:", response);
+      //source.refreshAsync();
+      $("#message").html(response);
+    });
+    request_body.changes = [];
+    data_source.refreshAsync();
+  }
+}
+
+function clear_table(){
   $("table.src-table").remove();
 }
 
-function show_data_in_table(source, headers, data){
+function show_data_in_table(headers, data){
+  clear_table();
   //console.log("showing in table", headers, data);
   let table_html = "";
   let table_row = "";
@@ -110,7 +122,7 @@ function show_data_in_table(source, headers, data){
     }
     table_html+="<tr>"+table_row+"</tr>";
   }
-  $("#table-container").append('<table data-source="'+source+'" class="src-table">'+table_html+'</table>');
+  $("#table-container").append('<table class="src-table">'+table_html+'</table>');
 }
 
 $(document).ready(() => {
